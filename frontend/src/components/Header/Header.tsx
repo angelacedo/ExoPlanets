57f49a8a3079126a48d6741d9604552bb79fe5c0
AAM <angel.acedo2003@gmail.com>
2025-01-05 22:46:39 +0100

    change <meta> tags and logo image size

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ function Header()
 
   return (
     <header className='flex w-full border-b-[1px] shadow-md' >
-      <div className='w-[20%]'>
-        <img src={logo} width={70} alt="planet logo" className='ml-3' />
+      <div className='w-[20%] px-3'>
+        <img src={logo} width={70} height="100%" alt="planet logo" className='ml-3' />
       </div>
       <nav className="font-thin flex items-center">
         <Link className='mx-auto mr-5' color='var(--generic-text-color)' to="/">DashBoard</Link>
